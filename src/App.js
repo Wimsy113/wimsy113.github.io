75b5ec8eece78dda3b9342f2e11556c1deffc59e
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2025-01-06 13:55:20 -0500

    Changes to gh-pages

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   }, []);
 
   return (
-  <BrowserRouter basename="/wimsy.github.io">
+  <BrowserRouter basename="/wimsy113.github.io">
     <div className="app-body">
       <nav className={`nav ${isSticky ? "sticky" : ""}`}>
         <ul className="nav-list">
